2a542252e3fd46f69556013ba34202667ef0391e
Oscar Losada <oscarlosu@gmail.com>
2015-11-10 11:42:21 +0100

    .

--- GameEngines2015/Assets/Scripts/TestRectangleGrid.cs
+++ GameEngines2015/Assets/Scripts/TestRectangleGrid.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class TestRectangleGrid : MonoBehaviour
 {
@@ -8,6 +8,9 @@ public class TestRectangleGrid : MonoBehaviour
     public int Width, Depth, Height;
     public int X, Y, Layer;
 
+    //public short[] Matrix;
+    public int size;
+
     // Use this for initialization
     void Start()
     {
@@ -15,6 +18,13 @@ public class TestRectangleGrid : MonoBehaviour
         MyGrid.SetGridSize(Width, Depth, Height);
         MyGrid.FillRect(TestObject, 0, 0, 0, Width - 1, Depth - 1, Height - 1);
         //MyGrid.SetGridSize(1, 2, 1);
+        //Matrix = new short[size];
+        //for(int i = 0; i < size; ++i)
+        //{
+        //    Matrix[i] = new GameObject();
+        //}
+        
+
     }
 
     // Update is called once per frame
