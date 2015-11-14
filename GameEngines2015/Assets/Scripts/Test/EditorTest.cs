cd1c9a88f95999eb210b14a89e181a0fd25db7b9
Phillip Phoelich <pphoelich@gmail.com>
2015-11-14 15:06:52 +0100

    Editor scripting test.

---
+++ GameEngines2015/Assets/Scripts/Test/EditorTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+public class EditorTest : MonoBehaviour
+{
+
+    public EditorObjectTest EOTest;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Debug.Log("Start");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    Debug.Log("Update");
+
+	    foreach (var i in EOTest.IntArray)
+	    {
+	        Debug.Log(i);
+	    }
+    }
+}
