6c20d9bfc27b6bc53231d478041e8999e3002073
Phillip Phoelich <pphoelich@gmail.com>
2015-11-19 11:39:48 +0100

    Old code used for testing added.

--- GameEngines2015/Assets/Scripts/Demo&Testing/TestRectangleGrid.cs
+++ GameEngines2015/Assets/Scripts/Demo&Testing/TestRectangleGrid.cs
@@ -14,7 +14,7 @@ public class TestRectangleGrid : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        MyGrid.SetGridSize(NX, NY, NLayer);
+        /*MyGrid.SetGridSize(NX, NY, NLayer);
         
         for (int y = 0; y < NY; y++)
         {
@@ -46,8 +46,53 @@ public class TestRectangleGrid : MonoBehaviour
                     
                 }
             }
+        }*/
+
+
+        /**********************
+        * Old test code.
+        **********************/
+
+        MyGrid.SetGridSize(NX, NY, NLayer);
+        for (int layer = 0; layer < NLayer; layer++)
+        {
+            for (int y = 0; y < NY; y++)
+            {
+                for (int x = 0; x < NX; x++)
+                {
+                    MyGrid.Place(0, x, y, layer);
+                }
+            }
         }
+
+        //MyGrid.AddLayer();
+        //Handler.Load();
+        MyGrid.FillRect(1, 0, 0, NLayer - 1, NX - 1, NY - 1, NLayer - 1);
+        //MyGrid.SwapLayers(0, Height - 1);
+        //MyGrid.RemoveLayer(Height);
+        //MyGrid.RemoveLayer(Height-1);
+        //MyGrid.RemoveLayer(Height-2);
+        //MyGrid.RemoveLayer(Height - 3);
+        //MyGrid.MoveRect(5, 5, Height - 3, Width - 1, Depth - 1, Height, 0, 0, 0);
+        //MyGrid.Place(1, 5, 5, Height);
+        //Handler.UpdateCell(5, 5, Height);
+
+        //MyGrid.Move(0, 0, 0, 0, 0, Height - 1);
+        //MyGrid.Swap(0, 0, 0, 0, 0, Height - 1);
+        //MyGrid.Remove(0, 0, Height - 1);
+        //MyGrid.MoveRect(0, 0, 0, 0, 0, 0, 0, 0, Height - 1);
+        //MyGrid.Place(0, 5, 5, Height-1);
+        //MyGrid.AddLayer();
+        //MyGrid.Place(0, 5, 5, Height);
+        //MyGrid.RemoveRect(0, 0, Height - 1, 4, 4, Height - 5);
+
+        // Interesting small patch of grass with tall walls of dirt.
+        MyGrid.RemoveRect(0, 0, NLayer - 1, 4, 4, NLayer - 5);
+        MyGrid.FillRect(1, 0, 5, NLayer - 5, 5, 5, NLayer - 1);
+        MyGrid.FillRect(1, 5, 5, NLayer - 5, 5, 0, NLayer - 1);
+
     }
+
 	float fBm(int iterations, float x, float y)
 	{
 		float value = 0;
